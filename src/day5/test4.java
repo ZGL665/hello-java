414d42cb6861ba559fc5f51dfec9300b699cba1b
bwf1 <bwf1.com>
2021-08-13 14:19:28 +0800

    test4

---
+++ src/day5/test4.java
@@ -0,0 +1,27 @@
+package day5;
+
+import java.util.Scanner;
+
+public class test4 {
+	
+	public static void main() {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		String num1=sc.nextLine();
+		
+		int num11=Integer.parseInt(num1);
+		
+		
+		if (num11==1) {
+			
+			
+			System.out.println("打印一");
+			sc.close();
+		}
+		
+		
+		
+	}
+
+}
