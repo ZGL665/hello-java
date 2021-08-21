4b6c4f2334916e2027232d994bdd835dfa9f3dec
bwf1 <bwf1.com>
2021-08-22 00:50:47 +0800

    课时88结束

--- src/daysix/daySix.java
+++ src/daysix/daySix.java
@@ -1,10 +1,47 @@
 package daysix;
 
+import java.util.Scanner;
+
 public class daySix {
+
 
 	public static void main(String[] args) {
 		System.out.println("周六复习");
-
+		
+		System.out.println(7);
+		System.out.println(07);
+		System.out.println(010);
+		System.out.println(0xe);
+		System.out.println("浮点型");
+		System.out.println(3.14159265358979323846f);
+		System.out.println(3.14159265358979323846d);
+		System.out.println(3.14e2);
+		System.out.println(3.14e6);
+		System.out.println(0.3f+0.6f);
+		System.out.println(0.3+0.6);
+		System.out.println("布尔类型");
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println("字符型");
+		System.out.println('A');
+		System.out.println(' ');
+		System.out.println('文');
+		System.out.println('A'+1);
+		System.out.println('a'+1);
+		System.out.println("转义字符");
+		System.out.println("上面的");
+		System.out.println("\r\n");
+		//System.out.println('\r');
+		System.out.println(' ');
+		System.out.println('\0');
+		System.out.println('\\');
+		System.out.println('\t');
+		System.out.println("下面的");
+		System.out.println('\65');		
+		
+		Scanner  sc=new Scanner(System.in);
+		
+		
 	}
 
 }
