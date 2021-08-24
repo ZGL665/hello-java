bae26871afff8e8c76d8f4734b5d54fd07289452
bwf1 <bwf1.com>
2021-08-24 14:56:12 +0800

    8.16   java基础部分结束提交笔记

--- src/day8_16/Dog.java
+++ src/day8_16/Dog.java
@@ -1,24 +1,33 @@
 package day8_16;
 
 public class Dog {
-	
-	
+
 	public String name;
 	public int id;
-	
-	
+
 	public Dog() {
-		
+
+		id = 100;
+		name = "旺财";
 		System.out.println("自己定义的无参构造函数");
+
+	}
+
+	public Dog(int id) {
+
+		this.id = id; // 成员变量的初始化    this.成员变量=局部变量
+
+		System.out.println("dog的编号" + id);
+
+	}
+	public Dog(int a,int b,int c) {
+		
+		this(c);
+		
+		System.out.println("调用这个类中的构造方法"+c);
 		
 		
 	}
-	public Dog(int bh) {
-		
-		System.out.println("带参数的构造函数");
-		System.out.println("编号："+bh);
-		
-		
-	}
+	
 
 }
---
+++ src/day8_16/Test2.java
@@ -0,0 +1,17 @@
+package day8_16;
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		
+		Dog d = new Dog(20, 25, 30);
+		
+		
+		System.out.println(d.id);
+		
+		
+		
+	}
+
+
+}
