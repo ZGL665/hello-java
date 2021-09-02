e3e462e051ff266fec3f95f3eda805e1250448fd
bwf1 <bwf1.com>
2021-09-02 23:27:25 +0800

    a

--- src/day8_27/Test1.java
+++ src/day8_27/Test1.java
@@ -1,10 +1,26 @@
 package day8_27;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-
+		User u = new User();
+		u.setId(1);
+		u.setName("bwf");
+		
+		
+		File file=new File("D:/myfiles/user.txt");
+		OutputStream out=new FileOutputStream(file);
+		
+		
+		
+		
+		
 		
 		
 
--- src/day8_27/User.java
+++ src/day8_27/User.java
@@ -4,6 +4,18 @@ public class User {
 	
 	private String name;
 	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	
 	
